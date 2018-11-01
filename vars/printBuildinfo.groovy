d07f8f6c4305eec59c64cdc2bd882815e5278177
khmelevskikh <khmelevskikh@gmail.com>
2018-11-01 20:19:23 +0400

    Update printBuildinfo.groovy

--- vars/printBuildinfo.groovy
+++ vars/printBuildinfo.groovy
@@ -1,7 +1,5 @@
 #!/usr/bin/env groovy
-import hudson.model.*
-import hudson.FilePath
-    
+   
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -14,11 +12,6 @@ def call(body) {
     if (env.param1 == 'One default') {
         echo "Param1 is default"
     }
-    
-    git branch: 'master',
-        credentialsId: 'nc_git',
-        url: 'git@git.netcracker.com:khmelevskikh/test.git'
-
     sh "chmod +x $WORKSPACE/test_instances.sh"  
 
     return this
