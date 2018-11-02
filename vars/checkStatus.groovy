5b5d57c2e37b2c9efadcdded758f46367adc2a5b
khmelevskikh <khmelevskikh@gmail.com>
2018-11-02 11:05:29 +0400

    Update checkStatus.groovy

--- vars/checkStatus.groovy
+++ vars/checkStatus.groovy
@@ -4,9 +4,7 @@ def call(body) {
     echo "Check status"
 
     echo env.JOB_NAME
-    echo env.RUN_DISPLAY_URL
-    
-    build job: 'pipe'
+    echo env.RUN_DISPLAY_URL    
 
     currentBuild.result = 'SUCCESS' //FAILURE to fail
     return this
