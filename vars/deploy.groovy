39c6794db2a2d4db3a56b54a0481d4feb4ec79c0
khmelevskikh <khmelevskikh@gmail.com>
2018-11-07 13:42:40 +0400

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,10 +1,14 @@
 #!/usr/bin/env groovy
+import java.io.File;
 
 def call(body) {
     echo "Start Deploy"
 
     new Deployer(script:this).run()
-
+    
+    File theFile = new File("$WORKSPACE/Jenkinsfile");
+    log.info "Param1 is: " + theFile.getParent()
+    log.info "Param1 is: " + theFile.getName()
     echo "Deployed"
     currentBuild.result = 'SUCCESS' //FAILURE to fail
 
